657cc839319a95a24b4e5f072313de227d65d3ec
patrick-foulis <125148805+patrick-foulis@users.noreply.github.com>
2025-01-10 16:21:05 -0500

    Add files via upload
    Unifnished but set up

---
+++ Main.py
@@ -0,0 +1,6 @@
+# Higher or Lower - pygame version 
+# Main program 
+# 4 - Load Assets: image(s), sound(s), etc. 
+import pywidgets
+background = pywidgets.Image(window, (0,0), 'images/background.png')
+newGameButton = pywidgets.Textbutton(window, (20,530), 'New Game', width=100, height=100)
